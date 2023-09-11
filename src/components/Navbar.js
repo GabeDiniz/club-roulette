c0bdcebce799f34a7eea8ca7b34045d5d1dc60ad
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-09-10 22:25:08 -0400

    Init: components / tailwind / setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  // Define any component-specific variables or states here
+
+  // You can write functions or methods specific to this component here
+  return (
+    <div className="">
+      {/* JSX content of the component */}
+      <h1>Navbar</h1>
+      {/* Add your HTML/JSX content here */}
+    </div>
+  );
+};
+
+export default Navbar;
