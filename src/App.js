d78ee98af08a0ac393b8281e0e4c847f6e45146b
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-09-14 20:23:12 -0400

    Remove: unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 // Components
@@ -26,20 +25,6 @@ function App() {
     <div className="App">
       <Navbar />
       <SpinningWheel options={clubOptions} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
