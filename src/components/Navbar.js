f18f6798b6763f305aa4560ae2be5a95488e9998
Gabriel Diniz <gabriel.sundiniz@gmail.com>
2023-09-15 11:37:59 -0400

    Navbar: add banner image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from "react";
+import banner from "./public/golf-course.jpg";
 
 const Navbar = () => {
   return (
     <div className="flex justify-center place-items-center h-14">
       {/* JSX content of the component */}
-      <h1 className="uppercase font-bold">⛳ Welcome to Club Roulette</h1>
+      <img className="w-full h-full object-cover" src={banner} alt="Banner" />
+      <span className="absolute w-full h-14 bg-main opacity-50"></span>
+      <h1 className="absolute uppercase font-bold text-white">
+        ⛳ Welcome to Club Roulette
+      </h1>
       {/* Add your HTML/JSX content here */}
     </div>
   );
