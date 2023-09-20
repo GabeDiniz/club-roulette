59c138278326018425addada845307896ab207d7
gdiniz <gabriel.sundiniz@gmail.com>
2023-09-20 14:19:51 -0400

    style: club and spin buttons

--- src/components/SpinningWheel.js
+++ src/components/SpinningWheel.js
@@ -19,13 +19,13 @@ const SpinningWheel = ({ options }) => {
 
   return (
     <div className="flex flex-col bg-main h-screen text-white">
-      <h1 className="mt-4">Club Options:</h1>
+      <h1 className="my-4">Club Options:</h1>
       {/* The Wheel style={{ transform: `rotateX(${spinNumber}deg)` }}*/}
-      <div className="flex flex-wrap justify-evenly mx-2 mt-2">
+      <div className="flex flex-wrap mx-1">
         {options.map((option, index) => (
           <div
             key={index}
-            className=" bg-rose-600 border-rose-300 rounded-md border-2 px-2 py-1 mx-2 mb-2"
+            className=" bg-rose-400 border-white rounded-md border-b-2 px-2 py-1 mx-2 mb-2"
           >
             {option}
           </div>
@@ -34,7 +34,7 @@ const SpinningWheel = ({ options }) => {
 
       {/* Button to spin the wheel */}
       <button
-        className="bg-rose-600 border-rose-300 border-2 rounded-md mt-2 mx-auto px-4 py-1"
+        className="bg-rose-600 border-white border-2 rounded-md mt-2 mx-auto px-8 py-2 hover:bg-rose-800"
         onClick={spinWheel}
       >
         Spin
@@ -42,7 +42,7 @@ const SpinningWheel = ({ options }) => {
 
       {/* Display the club */}
       {selectedOption && (
-        <p className="font-bold text-lg">Club: {selectedOption}</p>
+        <p className="font-bold text-lg mt-4">Club: {selectedOption}</p>
       )}
       {/* <p>Debugging Random Number: {spinNumber}</p> */}
     </div>
