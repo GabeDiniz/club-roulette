46df731654f26737ec808004d11542551d13e9e0
gdiniz <gabriel.sundiniz@gmail.com>
2023-09-21 16:30:21 -0400

    add: input/button for adding clubs

--- src/components/SpinningWheel.js
+++ src/components/SpinningWheel.js
@@ -34,7 +34,7 @@ const SpinningWheel = ({ options }) => {
 
       {/* Button to spin the wheel */}
       <button
-        className="bg-rose-600 border-white border-2 rounded-md mt-2 mx-auto px-8 py-2 hover:bg-rose-800"
+        className="bg-rose-600 border border-white rounded-md mt-2 mx-auto px-8 py-2 hover:bg-rose-800"
         onClick={spinWheel}
       >
         Spin
@@ -45,6 +45,20 @@ const SpinningWheel = ({ options }) => {
         <p className="font-bold text-lg mt-4">Club: {selectedOption}</p>
       )}
       {/* <p>Debugging Random Number: {spinNumber}</p> */}
+
+      <input
+        className="mt-8 h-8 mx-auto px-2 rounded-md border-2 border-white bg-black bg-opacity-40 focus:border-main"
+        type="text"
+        id="add_club"
+        name="add_club"
+        placeholder="Add Club"
+      />
+      <button
+        className="bg-rose-600 border border-white rounded-md mt-2 mx-auto px-8 py-2 hover:bg-rose-800"
+        // onClick={}
+      >
+        Add Club
+      </button>
     </div>
   );
 };
