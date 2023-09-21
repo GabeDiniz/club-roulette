3bab3cece92f00fc35b0d57049ecd0f7bf66bcdd
gdiniz <gabriel.sundiniz@gmail.com>
2023-09-21 16:57:51 -0400

    WIP: add club form

--- src/components/SpinningWheel.js
+++ src/components/SpinningWheel.js
@@ -17,6 +17,12 @@ const SpinningWheel = ({ options }) => {
     setSelectedOption(options[randomNumber]);
   };
 
+  const handleAddClub = (event) => {
+    event.preventDefault();
+    console.log("testing");
+    return false;
+  };
+
   return (
     <div className="flex flex-col bg-main h-screen text-white">
       <h1 className="my-4">Club Options:</h1>
@@ -45,20 +51,22 @@ const SpinningWheel = ({ options }) => {
         <p className="font-bold text-lg mt-4">Club: {selectedOption}</p>
       )}
       {/* <p>Debugging Random Number: {spinNumber}</p> */}
-
-      <input
-        className="mt-8 h-8 mx-auto px-2 rounded-md border-2 border-white bg-black bg-opacity-40 focus:border-main"
-        type="text"
-        id="add_club"
-        name="add_club"
-        placeholder="Add Club"
-      />
-      <button
-        className="bg-rose-600 border border-white rounded-md mt-2 mx-auto px-8 py-2 hover:bg-rose-800"
-        // onClick={}
-      >
-        Add Club
-      </button>
+      <form className="flex flex-col" onSubmit={handleAddClub}>
+        <input
+          className="mt-8 h-8 mx-auto px-2 rounded-md border-2 border-white bg-black bg-opacity-40 focus:border-main"
+          type="text"
+          id="newclub"
+          name="newclub"
+          placeholder="Add Club"
+        />
+        <button
+          type="submit"
+          className="bg-rose-600 border border-white rounded-md mt-2 mx-auto px-8 py-2 hover:bg-rose-800"
+          // onClick={}
+        >
+          Add Club
+        </button>
+      </form>
     </div>
   );
 };
