f5ea24f7d88d9f878bce7e39e8916aafeee54757
gdiniz <gabriel.sundiniz@gmail.com>
2023-10-04 14:57:52 -0400

    style: navbar sizing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ import banner from "./public/golf-course.jpg";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-center place-items-center h-14">
+    <div className="flex justify-center place-items-center h-24">
       {/* JSX content of the component */}
       <img className="w-full h-full object-cover" src={banner} alt="Banner" />
       <span className="absolute w-full h-14 bg-main opacity-50"></span>
-      <h1 className="absolute uppercase font-bold text-txt_main">
+      <h1 className="absolute text-xl uppercase font-bold text-txt_main">
         ⛳ Welcome to Club Roulette
       </h1>
       {/* Add your HTML/JSX content here */}
